d70697a84c53944e7d24417886c760812d81b9d8
poudelsanchit <snchtpdl12@gmail.com>
2025-12-20 21:23:10 +0545

    fixed the auth errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { UserProvider } from "./context/userContext.tsx";
+import axios from "axios";
+
+// Configure axios base URL
+axios.defaults.baseURL = "http://localhost:3000";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
