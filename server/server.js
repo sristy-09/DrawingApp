f697cd8412548340ccac0e080dd6cab28b8a8a07
sristy-09 <dhakalshristi09@gmail.com>
2026-03-23 15:17:03 +0545

    Added the route for pages in the backend.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import user from "./routes/auth.js";
 import board from "./routes/board.js";
+import page from "./routes/page.js";
 import "./config/passport.js";
 import path from "path";
 
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/auth", user);
 app.use("/board", board);
+app.use("/board", page);
 
 // Serve frontend build
 app.use(express.static(path.join(__dirname, "/client/dist")));
